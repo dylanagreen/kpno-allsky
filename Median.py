4ac0838ccd4ab6006f25982e266159e13c8cc22a
LegoBricker <dylan@mygreenfamily.us>
2017-07-15 16:26:51 -0700

    Median now finds 3 medians: .3s, 6s and all images.

--- Median.py
+++ Median.py
@@ -64,6 +64,15 @@ def download_all_date(date):
     
     print('All photos downloaded for ' + date)
 
+# Gets the expousre time of an image. 225 is the greyscale value for the yellow in the image.
+# The pixel chosen is yellow for .3s and "not yellow" for 6s.
+def get_exposure(image):
+    if image[19][174] == 225:
+        return .3
+    else:
+        return 6
+
+
 def median_all_date(date):
     
     # I've hard coded the files for now, this can be changed later.
@@ -77,30 +86,63 @@ def median_all_date(date):
         print('Are you sure you downloaded images?')
         exit()
     
-    # Create the final image array first.
-    finalimg = np.zeros((512,512))
+    # Directory for the files to save to later
+    filedir = 'Images/Median/' + date
+    
+    if not os.path.exists(filedir):
+        os.makedirs(filedir)
+    
+    # Create the final image arrays first.
+    finalimgall = np.zeros((512,512))
+    finalimg03 = np.zeros((512,512))
+    finalimg6 = np.zeros((512,512))
 
     # Boolean for if we have our temp image array yet.
-    superimg = np.zeros((1,1,1))
-    exists = False
+    superimgall = np.zeros((1,1,1))
+    existsall = False
+    
+    superimg03 = np.zeros((1,1,1))
+    exists03 = False
+    
+    superimg6 = np.zeros((1,1,1))
+    exists6 = False
+    
+    # I don't want to construct a new one every loop later on.
+    zeroes = np.zeros((1,1,1))
+    
     for file in files:
         # Make sure we look in the directory lol.
-        file = dir + file
+        file = directory + file
     
         # We have to reshape the images so that the lowest level single value is a 1D array rather than just a number.
         # This is so when you concat the arrays it actually turns the lowest value into a multivalue array.
         img = ndimage.imread(file, mode = 'L')
         temp = img.reshape(512, 512, 1)
-    
-        # Make the super image have the correct dimensions and starting values.
-        if exists:
+        
+        exposure = get_exposure(img)
+        
+        # Make the super image have the correct dimensions and starting values. Concats if it already does.
+        if existsall:
             # Concatenates along the color axis
-            superimg = np.concatenate((superimg,temp), axis=2)
+            superimgall = np.concatenate((superimgall,temp), axis=2)
         else:
             # Since we run this only once this shortcut will save us fractions of a second!
-            superimg = temp
-            exists = True
+            superimgall = temp
+            existsall = True
         
+        # Exposure specific medians.
+        if exposure == 6:
+            if exists6:
+                superimg6 = np.concatenate((superimg6,temp), axis=2)
+            else:
+                superimg6 = temp
+                exists6 = True
+        else:
+            if exists03:
+                superimg03 = np.concatenate((superimg03,temp), axis=2)
+            else:
+                superimg03 = temp
+                exists03 = True
 
 
     # i and j are counters here for positions in the final array.
@@ -109,15 +151,35 @@ def median_all_date(date):
     i = 0
     j = 0
 
-    for row in superimg:
+    # I've condensed the 3 images into 1 loop, since they all have the same dimensions in x and y.
+    for row in superimgall:
         for column in row:
             # Need to sort before taking the median.
             # Median is just the middle item in the array so find the length, then integer divide by 2.
             # This keeps it an integer but still rounds up if the length was odd. (i.e. median in 5 items is item 3 => index 2 => 5 // 2)
             sorted = np.sort(column)
             pos = sorted.shape[0] // 2 
-            finalimg[i,j] = sorted[pos]
+            finalimgall[i,j] = sorted[pos]
             #print(sorted[pos])
+            
+            # This if block is required if there are no images of that exposure time.
+            # It'll explode if we don't verify that we've actually added an image to the super array.
+            if np.array_equal(superimg03, zeroes):
+                j += 1 # I need this to actually increment every loop after performing math lol.
+                continue
+            else:
+                sorted = np.sort(superimg03[i,j])
+            pos = sorted.shape[0] // 2 
+            finalimg03[i,j] = sorted[pos]
+            
+            if np.array_equal(superimg6, zeroes):
+                j += 1
+                continue
+            else:
+                sorted = np.sort(superimg6[i,j])
+            pos = sorted.shape[0] // 2
+            finalimg6[i,j] = sorted[pos]
+            
             j += 1
         i += 1
         j = 0
@@ -134,25 +196,35 @@ def median_all_date(date):
     # Then add axes to figure
     axes.set_axis_off()
     figure.add_axes(axes)
+    
 
-    # Adds the image into the axes and displays it
+    filename = filedir + '/All'
     # cmap is required here since I did the images in grayscale and imshow needs to know that.
-    axes.imshow(finalimg, cmap = 'gray')
-
-    filename = 'Images/' + date + '-median.png'
-    # DPI chosen to have resultant image be the same size as the originals. 128*4 = 512
+    axes.imshow(finalimgall, cmap = 'gray')
     plot.savefig(filename, dpi = 128)
     
-    print('Median image complete for ' + date)
+    filename = filedir + '/03s'
+    axes.imshow(finalimg03, cmap = 'gray')
+    plot.savefig(filename, dpi = 128)
+    
+    filename = filedir + '/6s'
+    axes.imshow(finalimg6, cmap = 'gray')
+    plot.savefig(filename, dpi = 128)
+    
+    print('Median images complete for ' + date)
     
     # Show the plot
-    plot.show()
+    #plot.show()
     
 
+    
+    
 
 date = '20170710'
-download_all_date(date)
+#download_all_date(date)
 median_all_date(date)
+
+#get_exposure(date)
 
 
 
