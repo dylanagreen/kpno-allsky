c07bca12604972d6d579aeb717ee4aabcf42d5f2
LegoBricker <dylan@mygreenfamily.us>
2017-07-17 16:38:48 -0700

    Change r -> alt conversion.

--- Coordinates.py
+++ Coordinates.py
@@ -2,7 +2,7 @@ import math
 from astropy.coordinates import SkyCoord, EarthLocation
 from astropy.time import Time
 from astropy import units as u
-
+import numpy as np
 
 
 # This is the latitutde/longitude of the camera
@@ -15,7 +15,7 @@ cameraearth = EarthLocation(lat = cameraloc[0] * u.deg, lon = cameraloc[1] * u.d
 center = (256, 252)
 
 # The point for which we're doing this mathmagic.
-point = (255, 252)
+point = (300, 300)
 
 # Point adjusted based on the center being at... well... the center. And not the top left. In case you were confused.
 pointadjust = (point[0] - center[0], point[1] - center[1])
@@ -47,23 +47,20 @@ if r >= 239:
     alt = 90
 else:
     #r = 238 # Debug line
-    r = r * 0.0483264 # Magic pixel to mm conversion rate
-
-
-    # This is probably very not accurate right now. 
-    # I used a table of r -> theta values for this lens and then found a regression for it
-    a = 7.138255950800197
-    b = 0.021488582426739067
-    c = 0.0002825039064767284
-    alt =  a*r + b*math.pow(r,2) + c*math.pow(r,4)
-
-# The alt computed is down from the vertical, rather than up from the horizontal. Easy fix.
-alt = 90 - alt
+    r = r * 11.6 / 289# Magic pixel to mm conversion rate
+    
+    rpoints = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 11.6]
+    thetapoints = [0, 3.58, 7.17, 10.76, 14.36, 17.98, 21.62, 25.27, 28.95, 32.66, 36.40, 40.17, 43.98, 47.83, 51.73, 55.67, 59.67, 63.72, 67.84, 72.03, 76.31, 80.69, 85.21, 89.97,90]
+   
+   # 90- turns the angle from measured from the vertical to measured from the horizontal.
+   # This interpolates the value from the two on either side of it.
+    alt = 90 - np.interp(r, xp = rpoints, fp = thetapoints)
 
 print('Azimuthal angle = ' + str(az))
 print('Altitude angle = ' + str(alt))
 
 # obstime?
+
 
 time = Time('2017-7-12 23:00:00')
 
