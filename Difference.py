752ae0c9c28ef59c72fa861928e11080e1082406
LegoBricker <dylan@mygreenfamily.us>
2017-08-23 09:57:01 -0700

    Add variable size to Difference.py.

--- Difference.py
+++ Difference.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plot
 # Reads the two images into ndarrays.
 # Importing in RGB mode makes it ignore the A layer.
 # If we suddenly have transparency I might have a problem.
-img1 = ndimage.imread('unnamed.png', mode = 'RGB')
-img2 = ndimage.imread('unnamed-2.png', mode = 'RGB')
+img1 = ndimage.imread('conv.png', mode = 'RGB')
+img2 = ndimage.imread('conv2.png', mode = 'RGB')
 
 # Difference image. Assumed literal mathematical difference for now.
 # I encountered a problem previously, in that I assumed the type of the array would dynamically change.
@@ -17,11 +17,15 @@ img2 = ndimage.imread('unnamed-2.png', mode = 'RGB')
 # I then have to cast back to uints because imshow works differently on uint8 and int16.
 diffimg = np.uint8(abs(np.int16(img1) - np.int16(img2)))
 
-print(diffimg) # Debug line
+#print(diffimg) # Debug line
+
+dpi = 128
+y = diffimg.shape[0] / dpi
+x = diffimg.shape[1] / dpi
 
 # Generate Figure and Axes objects.
 figure = plot.figure()
-figure.set_size_inches(4,4) # 4 inches by 4 inches
+figure.set_size_inches(x,y) # 4 inches by 4 inches
 axes = plot.Axes(figure,[0.,0.,1.,1.]) # 0 - 100% size of figure
 
 
@@ -35,7 +39,7 @@ axes.imshow(diffimg)
 
 
 # DPI chosen to have resultant image be the same size as the originals. 128*4 = 512
-plot.savefig("blah.png", dpi = 128)
+plot.savefig("blah.png", dpi = dpi)
 
 # Show the plot
 plot.show()
