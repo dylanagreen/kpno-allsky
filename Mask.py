33935aec4a83ede9865eb8475c582320d2a942ab
LegoBricker <dylan@mygreenfamily.us>
2017-11-26 18:08:03 -0800

    Rewrite Mask.py to increase run speed.
    - Mask is a speed bottleneck for transforming and cloud contrasting.
    - This rewrite firstly makes generate methods return the mask as a binary
    image rather than as a list of pixels. This allows apply_mask to simply
    multiply the mask with the image to be masked.
    - This then allows generate to load a saved mask and then simply convert
    binary 1-0 (rather than 255-0) and use that.
    - This saves time, since generating a mask from scratch requires looping
    through every pixel in 6 512x512 images (rather slow). The new method loads
    the mask and immediately returns it.
    -I have added a boolean value that forces generate to use the old loop method
    everytime it generates, ignoring a saved Mask.py, rather than just the first
    time it's run.

--- Mask.py
+++ Mask.py
@@ -17,7 +17,11 @@ def generate_clean_mask():
     files = os.listdir(fileloc)
 
     tolerance = 160
-    mask = []
+    
+    # Sets up the mask to be the right size
+    file1 = fileloc + files[0]
+    img = ndimage.imread(file1, mode='L')
+    mask = np.zeros((img.shape[0],img.shape[1]))
 
     for file in files:
         file = fileloc + file
@@ -31,28 +35,47 @@ def generate_clean_mask():
                 # white-tolerance and white
                 # Y is first value as it's the row value
                 if img[y, x] >= (255 - tolerance):
-                    mask.append((x, y))
+                    mask[y, x] += 1
 
                 x += 1
             y += 1
             x = 0
 
-    # Get only the pixels that appear as "hot" in all of the images
-    # Essentially means we only want the duplicates in the list that are
-    # duplicated the same amount of times as the number of files. 
-    final = []
-    for item, num in Counter(mask).items():
-        if num == len(files):
-            final.append(item)
+    # Get only the pixels that appear as "hot" in ALL of the images.
+    # Set those to 0 to mask them.
+    final = np.where(mask >= np.amax(mask), 1, 0)
 
     return final
     
 # Gets the "clean" mask and then adds the horizon objects to it.
-def generate_mask():
-    mask = generate_clean_mask()
+# forcenew allows you to force this to disregarded a saved mask.
+def generate_mask(forcenew = False):
     
     # Read in the ignore image.
+    # I read this in first to make sure the Mask.png is the correct dimensions.
     ignore = ndimage.imread('Images/Ignore.png', mode='RGB')
+    
+    # If we've already generated and saved a mask, load that one.
+    # This speeds up code execution by a lot, otherwise we loop through 512x512
+    # pixels 6 times! With this we don't have to even do it once, we just load
+    # and go.
+    maskloc = 'Images/Mask.png'
+    if os.path.isfile(maskloc) and not forcenew:
+        mask = ndimage.imread(maskloc, mode = 'L')
+        # Converts the 255 bit loaded image to binary 1-0 image.
+        mask = np.where(mask == 255, 1, 0)
+        
+        # Have to compare these two separately, since ignore has a third color
+        # dimensions and mask.shape == ignore.shape would therefore always be
+        # False.
+        if mask.shape[0] == ignore.shape[0] and mask.shape[1] == ignore.shape[1]:
+            return mask
+        
+    
+    # Get the "clean" mask, i.e. the stars only ignore mask.
+    mask = generate_clean_mask()
+    
+
     
     y = 0
     x = 0
@@ -66,11 +89,14 @@ def generate_mask():
             # Ignore horizon objects (which have been painted pink)
             # Only want the horizon objects actually in the circle.
             # Avoids unnecessary pixels.
-            if np.array_equal(ignore[y, x], [244, 66, 235]) and r < 242:
-                mask.append((x, y))
+            if r < 242 and np.array_equal(ignore[y, x], [244, 66, 235]):
+                mask[y, x] = 1
             x += 1
         y += 1
         x = 0
+        
+    # If we've made a new mask, save it so we can skip the above steps later.
+    save_mask(mask)
     
     return mask
 
@@ -78,21 +104,18 @@ def generate_mask():
 # Saves a given mask as in image in the Images folder.
 def save_mask(mask):
     dest = 'Images/Mask.png'
-
-    newimg = np.zeros((512, 512))
-    newimg.fill(100)
-
-    for loc in mask:
-        newimg[loc[1], loc[0]] = 255
     
-    ImageIO.save_image(newimg, 'Mask.png', 'Images/', 'gray')
+    ImageIO.save_image(mask, 'Mask.png', 'Images/', 'gray')
 
 
 # Applies the mask to the image
 def apply_mask(mask, img):
     img = np.copy(img)
-    # For masked pixel set the pixel to 0
-    for loc in mask:
-        img[loc[1], loc[0]] = 0
+    
+    # Multiply mask with image to keep only non masked pixels.
+    # Invert the mask first since masked pixels are 1 and non masked are 0,
+    # And we want the non mask to be 1 so they get kept.
+    mask = 1 - mask
+    img = np.multiply(mask, img)
 
     return img
