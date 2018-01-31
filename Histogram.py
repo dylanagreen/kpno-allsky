1929b038e2026921b6c7e9d357945d4c7ca3eb25
Dylan Green <dylan@mygreenfamily.us>
2018-01-31 11:57:05 -0800

    Add moon fitting to predict radius.
    - Intention is to fit gaussians to the moons to find the image with the lowest moon radius.
    - I'm looking for the smoking gun image referred to a few commits ago.
    - Gaussian fits need to be tweaked slightly, currently seems to overestimate the size of the moon by
    quite a bit.

--- Histogram.py
+++ Histogram.py
@@ -13,6 +13,8 @@ from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astropy.time import Time
 import astropy.time.core as aptime
 from astropy import units as u
+from astropy.modeling import models, fitting
+import warnings
 
 from matplotlib.patches import Circle
 
@@ -34,7 +36,12 @@ def histogram(date, file):
         return
 
     # Gets the location of the moon.
-    circ = find_moon(img, date, file)
+    moonx, moony = find_moon(img, date, file)
+    r = fit_moon(img, moonx, moony)
+
+    # Sets up the patch, radius = 5, color = cyan
+    circ = Circle((moonx, moony), 100, fill = False)
+    circ.set_edgecolor('c')
 
     # Converts the 1/0 array to True/False so it can be used as an index.
     # Then applies it, creating a new "image" array that only has the inside the
@@ -51,12 +58,15 @@ def histogram(date, file):
     # Then add the image to the left axes with the moon circle.
     ax[0].set_axis_off()
     ax[0].imshow(img, cmap='gray')
-    ax[0].add_patch(circ)
+    #ax[0].add_patch(circ)
 
     # Creates the histogram with 256 bins (0-255) and places it on the right.
     bins = list(range(0,256))
     ax[1].hist(img1.flatten(), bins = bins, color = 'blue', log = True)
+
     #plt.show()
+
+    ax[0].text(0, -20, str(r), fontsize = 20)
 
     # Saving code.
     name = 'Images/Histogram/' + date + '/' + file
@@ -69,7 +79,7 @@ def histogram(date, file):
     plt.close()
 
 
-# Finds the moon and returns a circle at that location
+# Finds the moon position and returns a the x,y position of the moon.
 def find_moon(image, date, file):
 
     # Sets up location and time variables.
@@ -89,25 +99,88 @@ def find_moon(image, date, file):
     x, y = Coordinates.altaz_to_xy(alt, az)
     x, y = Coordinates.galactic_conv(x, y, az)
 
-    # Sets up the patch, radius = 5, color = cyan
-    circ = Circle((x, y), 5, fill = False)
-    circ.set_edgecolor('c')
+    return (x,y)
 
-    return circ
 
-#dates = os.listdir('Images/Original')
-#dates.remove('.DS_Store')
-#dates.remove('1')
-#dates = sorted(dates)
-#print(dates)
-#for date in dates:
+# Fits a gaussian to the moon and returns the estimated radius of the moon.
+# Radius of the moon is the average of the FWHM in the x and y directions.
+def fit_moon(img, x, y):
 
-date = '20160823'
+    # This block of code runs straight vertical from the center of the moon
+    # It gives a predicted rough radius of the moon, it starts counting at the
+    # first white pixel it encounters (because the center may be overflow black)
+    # and stops at the last white pixel. White here defined as > 250 greyscale.
+    yfloor = math.floor(y)
+    start = yfloor
+    count = False
+    size = 0
+    xfloor = math.floor(x)
+    for i in range(0,35):
+        start += 1
+        if not count and img[start, xfloor] >= 250:
+            count = True
+        elif count and img[start, xfloor] >= 250:
+            size += 1
+        elif count and img[start, xfloor] < 250:
+            break
 
-directory = 'Images/Original/' + date + '/'
-files = sorted(os.listdir(directory))
+    # Add some buffer pixels in case the center is black and the edges of the
+    # moon are fuzzed.
+    size = (size + 10) * 2
 
-for file in files:
-    histogram(date, file)
+    # Makes sure the lower/upper slices don't out of bounds error.
+    lowerx = xfloor - size if (xfloor - size > 0) else 0
+    lowery = yfloor - size if (yfloor - size > 0) else 0
+    upperx = xfloor + size if (xfloor + size < 511) else 511
+    uppery = yfloor + size if (yfloor + size < 511) else 511
 
-#test('r_ut012616s16560.png')
+    # Size of the moon enclosing square.
+    deltax = (upperx - lowerx)
+    deltay = (uppery - lowery)
+
+    # Creates two arrays, with the array values being the x or y coordinate of
+    # that location in the array.
+    y, x = np.mgrid[0:deltay, 0:deltax]
+
+    # Slices out the moon square and finds center coords.
+    z = img[lowery:uppery, lowerx:upperx]
+    midy = deltay / 2
+    midx = deltax / 2
+
+    # Gaussian fit, centered in square, stdev of 20 as a start.
+    stddev = 20
+    model_init = models.Gaussian2D(amplitude=200, x_mean=midx, y_mean=midy,
+                               x_stddev=stddev, y_stddev=stddev)
+    fit = fitting.LevMarLSQFitter()
+
+    with warnings.catch_warnings():
+        # Ignore model linearity warning from the fitter
+        warnings.simplefilter('ignore')
+        model = fit(model_init, x, y, z)
+
+    # /2 is average FWHM but FWHM = diameter, so divide by two again.
+    fwhm = (model.x_fwhm + model.y_fwhm) / 4
+
+    return fwhm
+
+
+
+if __name__ == "__main__":
+
+    # This code is here to loop through all currently downloaded dates.
+    #dates = os.listdir('Images/Original')
+    #dates.remove('.DS_Store')
+    #dates.remove('1')
+    #dates = sorted(dates)
+    #print(dates)
+    #for date in dates:
+
+    #date = '20171108'
+    date = '20170817'
+
+    directory = 'Images/Original/' + date + '/'
+    files = sorted(os.listdir(directory))
+
+    for file in files:
+        histogram(date, file)
+
--- ImageIO.py
+++ ImageIO.py
@@ -12,16 +12,16 @@ from scipy import ndimage
 # Saves an input image with the given name in the folder denoted by location.
 # If the image is greyscale, cmap should be 'gray'
 def save_image(img, name, location, cmap=None):
-    
+
     if not os.path.exists(location):
         os.makedirs(location)
-    
+
     # DPI chosen to have resultant image be the same size as the originals.
     # 128*4 = 512
     dpi = 128
     y = img.shape[0] / dpi
     x = img.shape[1] / dpi
-    
+
     # Generate Figure and Axes objects.
     figure = plot.figure()
     figure.set_size_inches(x, y)  # 4 inches by 4 inches
@@ -35,17 +35,17 @@ def save_image(img, name, location, cmap=None):
     # Adds the image into the axes and displays it
     # Then saves
     axes.imshow(img, cmap=cmap)
-    
+
     # If location was passed with / on the end, don't append another one.
     if not location[-1:] == '/':
         name = location + '/' + name
     else:
         name = location + name
-    
+
     # Append .png if it wasn't passed in like that already.
     if not name[-4:] == '.png':
         name = name + '.png'
-    
+
     # Print "saved" after saving, in case saving messes up.
     plot.savefig(name, dpi=dpi)
     print('Saved: ' + name)
@@ -197,7 +197,7 @@ def gray_and_color_image(file):
 # The second pixel is yellow in .03 and .002
 # but due to magic of if blocks that's ok.
 def get_exposure(image):
-    
+
     # Handles separate cases for greyscale and RGB images.
     if len(image.shape) == 2:
         pix1 = image[19, 174]
@@ -207,11 +207,11 @@ def get_exposure(image):
         pix1 = image[19, 174]
         pix1 = pix1[0] * 299/1000 + pix1[1] * 587/1000 + pix1[2] * 114/1000
         pix1 = math.floor(pix1)
-        
+
         pix2 = image[17, 119]
         pix2 = pix2[0] * 299/1000 + pix2[1] * 587/1000 + pix2[2] * 114/1000
         pix2 = math.floor(pix2)
-    
+
     if pix1 == 225:
         return 0.3
     if pix2 == 225:
@@ -220,7 +220,7 @@ def get_exposure(image):
         return 6
 
 
-# Returns the difference image between two images. 
+# Returns the difference image between two images.
 # Black areas are exactly the same in both, white areas are opposite.
 # Greyscale/color values are varying levels of difference.
 def image_diff(img1, img2):
